05374ee81f807b284fc8bd9d54cecab1c76d608e
zezehana17-spec <zezehana17@gmail.com>
2026-09-11 13:37:32 +0900

    Remove obsolete Zoom stylesheet import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import '@zoom/videosdk-ui-toolkit/dist/videosdk-ui-toolkit.css';
 
 export const metadata: Metadata = {
   title: 'Heart Talk Test',
